5fb2dc21227f8ffd67392cbfdf8ea3ecd1027ef2
marchetype <114883447+marchetype@users.noreply.github.com>
2022-12-29 13:04:44 -0500

    Started work with inquirer

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+let readmeArr = [];
 
-function Section(title, prompt, userInput) {
+function Section(title, prompt) {
     this.title = title;
     this.prompt = prompt;
-    this.userInput = userInput;
 }
 
-const description = new Section('Description', 'Please describe your project and its functionality:', '');
-const install = new Section('Installation', 'Please explain the installation process for your application:', '');
-const usage = new Section('Usage', 'Please enter usage information about your application:', '');
-const contribution = new Section('Contribution Guidelines', 'Please enter the guidelines for conribution to the project:', '');
-const testing = new Section('Test Instructions', 'Please enter instructions for testing:', '');
+let description = new Section('Description', 'Please describe your project and its functionality:');
+const install = new Section('Installation', 'Please explain the installation process for your application:');
+const usage = new Section('Usage', 'Please enter usage information about your application:');
+const contribution = new Section('Contribution Guidelines', 'Please enter the guidelines for conribution to the project:');
+const test = new Section('Test Instructions', 'Please enter instructions for testing:');
 // TODO: Create an array of questions for user input
-const stringInputQuestions = [description, install, usage, contribution, testing];
+const stringInputQuestions = [description,];
+
+function gatherInput() {
+    inquirer.prompt([
+    {
+        name: ``,
+        message:``,
+        type: 'input'
+    }
+    ])
+    .then(function(textInput)
+    {
+        readmeArr.push(description.title)
+        console.log(textInput);
+        readmeArr.push(textInput);
+        console.log(readmeArr);
+    });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -22,6 +39,7 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
+    gatherInput();
 }
 
 // Function call to initialize app
